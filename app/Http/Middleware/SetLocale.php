8930d3fe7f4eb2d0dc651e68a1a69b858d131bb2
med reda <mohammedreda3031@gmail.com>
2026-06-01 17:01:00 +0100

    Ajouter l'internationalisation FR EN

---
+++ app/Http/Middleware/SetLocale.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+
+class SetLocale
+{
+    public function handle(Request $request, Closure $next)
+    {
+        App::setLocale(session('locale', 'fr'));
+        return $next($request);
+    }
+}
